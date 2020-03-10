3b1c8ba4e41097a55f39099d5cb383419d566b14
Henry Haustein <henrydatei@web.de>
2020-03-10 10:43:23 +0100

    added fake userAgent

--- functions.py
+++ functions.py
@@ -1,15 +1,19 @@
 import requests
 
+userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
+
 def login(email, password):
     jsondata = {"email": email, "password": password, "strategy": "local"}
-    req = requests.post('https://api.upland.me/authentication', json=jsondata)
+    headers = {'User-Agent': userAgent}
+    req = requests.post('https://api.upland.me/authentication', json=jsondata, headers=headers)
     req.raise_for_status()
     return req.text
 
 def register(email, password, username):
     # using the promo-code "cryptoticker" will give you 6000 UPX instead of 3000 UPX at the beginning
     jsondata = {"email": email, "password": password, "username": username, "ab_token": "cryptotickerpromo1", "promo_code": "cryptoticker"}
-    req = requests.post('https://api.upland.me/users', json=jsondata)
+    headers = {'User-Agent': userAgent}
+    req = requests.post('https://api.upland.me/users', json=jsondata, headers=headers)
     req.raise_for_status()
     return req.text
 
@@ -20,63 +24,63 @@ def getDashboard(token):
     return req.text
 
 def renewVisa(token):
-    headers={"authorization": "Bearer "+token}
+    headers={"authorization": "Bearer "+token, 'User-Agent': userAgent}
     req = requests.post('https://api.upland.me/users/visa', headers=headers)
     req.raise_for_status()
     return req.text
 
 def myProperties(token):
-    headers={"authorization": "Bearer "+token}
+    headers={"authorization": "Bearer "+token, 'User-Agent': userAgent}
     req = requests.get('https://api.upland.me/yield/mine', headers=headers)
     req.raise_for_status()
     return req.text
 
 def myPropertiesDetailed(token):
-    headers={"authorization": "Bearer "+token}
+    headers={"authorization": "Bearer "+token, 'User-Agent': userAgent}
     req = requests.get('https://api.upland.me/properties/mine/detailed', headers=headers)
     req.raise_for_status()
     return req.text
 
 def getNearbyProperties(token,north,south,east,west):
-    headers={"authorization": "Bearer "+token}
+    headers={"authorization": "Bearer "+token, 'User-Agent': userAgent}
     parameters = {"north": north, "south": south, "east": east, "west": west}
     req = requests.get('https://api.upland.me/map', headers=headers, params=parameters)
     req.raise_for_status()
     return req.text
 
 def getNearbyPropertiesOnSale(token,north,south,east,west,sort_direction):
-    headers={"authorization": "Bearer "+token}
+    headers={"authorization": "Bearer "+token, 'User-Agent': userAgent}
     parameters = {"north": north, "south": south, "east": east, "west": west, "offset": 0, "limit": 20, "sort": sort_direction}
     req = requests.get('https://api.upland.me/properties/list-view', headers=headers, params=parameters)
     req.raise_for_status()
     return req.text
 
 def getPropertyInformation(token, prop_id):
-    headers={"authorization": "Bearer "+token}
+    headers={"authorization": "Bearer "+token, 'User-Agent': userAgent}
     req = requests.get('https://api.upland.me/properties/{}'.format(prop_id), headers=headers)
     req.raise_for_status()
     return req.text
 
 def findSuitableCollectionForProperty(token, prop_id):
-    headers={"authorization": "Bearer "+token}
+    headers={"authorization": "Bearer "+token, 'User-Agent': userAgent}
     req = requests.get('https://api.upland.me/properties/match/{}'.format(prop_id), headers=headers)
     req.raise_for_status()
     return req.text
 
 def getPriceForTeleportToProperty(token, prop_id):
-    headers={"authorization": "Bearer "+token}
+    headers={"authorization": "Bearer "+token, 'User-Agent': userAgent}
     req = requests.get('https://api.upland.me/teleports/price/{}'.format(prop_id), headers=headers)
     req.raise_for_status()
     return req.text
 
 def getNumberOfMessages(token):
-    headers={"authorization": "Bearer "+token}
+    headers={"authorization": "Bearer "+token, 'User-Agent': userAgent}
     req = requests.get('https://api.upland.me/messages/count', headers=headers)
     req.raise_for_status()
     return req.text
 
 def getStorePrices(token):
-    headers={"authorization": "Bearer "+token}
+    headers={"authorization": "Bearer "+token, 'User-Agent': userAgent}
     parameters = {"type": "upex"}
     req = requests.get('https://api.upland.me/store', headers=headers, params=parameters)
     req.raise_for_status()
